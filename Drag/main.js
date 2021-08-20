4d816cb4c6292aa8aa11ad9efcf92f85994b6ba7
gukoa4 <gukoa4@gmail.com>
2021-08-20 15:46:52 -0300

    Actulizado

--- Drag/main.js
+++ Drag/main.js
@@ -1,8 +1,19 @@
 "use strict";
 
 const circulo = document.querySelector(".circulo");
-let cuadrado = document.querySelector(".cuadrado");
+const cuadrado = document.querySelector(".cuadrado");
 
-circulo.addEventListener("dragstart",()=>console.log(1));
-circulo.addEventListener("drag",()=>console.log(2));
-circulo.addEventListener("dragend",()=>console.log(3));
+circulo.addEventListener("dragstart",(evt)=>{
+    evt.dataTransfer.setData("text-plain",evt.target.className);
+    console.log(evt.dataTransfer.getData("text-plain"))
+});
+//circulo.addEventListener("drag",()=>console.log("drag"));
+circulo.addEventListener("dragend",()=>console.log("dragend"));
+
+cuadrado.addEventListener("dragenter",()=>console.log("dragenter"));
+cuadrado.addEventListener("dragover",(evt)=>{
+    evt.preventDefault();
+    console.log("dragover")
+});
+cuadrado.addEventListener("drop",()=>console.log("drop"));
+cuadrado.addEventListener("dragleave",()=>console.log("dragleave"));
